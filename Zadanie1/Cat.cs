15a293c0c732699b9a14c98ad051a3863fc275c2
Sveta616 <152242882+Sveta616@users.noreply.github.com>
2024-11-01 19:22:29 +0500

    Add files via upload

---
+++ Zadanie1/Cat.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+
+    class Cat
+    {
+        private string name;
+        private double ves;
+        //проверка веса на цифры и значения(не равно 0 и не меньше)
+        public double Ves
+        {
+            get
+            {
+                return ves;
+            }
+            set
+            {
+                bool onlyCounts = true;
+            
+                string valuestr = value.ToString();
+
+                foreach (var ch in valuestr)
+                {
+                    if (!char.IsDigit(ch) && ch != '.') 
+                    {
+                        onlyCounts = false;
+                        break; 
+                    }
+                }
+
+                if (onlyCounts)
+                {
+                    ves = double.Parse(valuestr); 
+                }
+                else
+                {
+                    Console.WriteLine($"{valuestr} - нельзя буквы!!!");
+                }
+
+                if (value <= 0)
+                {
+                    Console.WriteLine("Вес не может быть нулевым или отрицательным!");
+                }
+                else
+                {
+                    ves = value;
+                }
+            }
+        }
+        //проверка имени на буквы
+        public string Name
+        {
+        
+            get
+            {
+                return name;
+            }
+        
+            set
+            {
+                bool OnlyLetters = true;
+              
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if (OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+            }
+        }
+        //констуктор
+        public Cat(string CatName, double CatVes)
+        {
+            Name = CatName;
+            Ves = CatVes;
+        }
+        // метод для опознания отдельно взятого кота
+        public void Meow()
+        {
+            Console.WriteLine($"{name}: МЯЯЯЯУ!!!!");
+            Console.WriteLine($"{ves} кг");
+        }
+    
+      
+    }
+}
---
+++ Zadanie1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Zadanie1
+{
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите кол-во котов");
+                int count = int.Parse(Console.ReadLine());
+                for(int i=0;i<count;i++)
+                {
+                    Console.WriteLine($"Введите имя {i + 1} кота");
+                    string CatName = Console.ReadLine();
+                    Console.WriteLine($"Введите вес {i + 1} кота");
+                    double CatVes = Convert.ToDouble(Console.ReadLine());
+                    Cat cat = new Cat(CatName, CatVes);
+                    cat.Meow();
+                    Console.ReadLine();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Введите данные корректно");
+                Console.ReadKey();
+            }
+        }
+    } 
+  
+}
